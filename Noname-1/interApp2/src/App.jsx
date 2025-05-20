b33f187548c9e911d6e920b8b1f8b65984e7f0e6
Blaze-trio <64141242+Blaze-trio@users.noreply.github.com>
2025-05-20 23:11:19 +0300

    Update Git LFS metadata for package.json and package-lock.json

--- Noname-1/interApp2/src/App.jsx
+++ Noname-1/interApp2/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3ff8c5f4dbc3c693f9616597d1e5ef7db6d6ef46c5de27f1171d52e7d6553d4
-size 8522
+oid sha256:f2234c9ae0379ff95b23c59a16892769915e786f456139cbc850a964c19f0d48
+size 9116
